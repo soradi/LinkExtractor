3801e10eb2f016b38a0ebc08aa660cb3bc5324ce
soradi <khsoradi@eng.cu.edu.eg>
2017-01-08 00:37:38 +0200

    Add files via upload

---
+++ LinkExtractor/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.txtThreadsCount = New System.Windows.Forms.TextBox()
+        Me.ButStart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtURL
+        '
+        Me.txtURL.Location = New System.Drawing.Point(107, 6)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(257, 20)
+        Me.txtURL.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "URL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Max Connections"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(12, 72)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 13)
+        Me.lblStatus.TabIndex = 3
+        '
+        'txtThreadsCount
+        '
+        Me.txtThreadsCount.Location = New System.Drawing.Point(107, 35)
+        Me.txtThreadsCount.Name = "txtThreadsCount"
+        Me.txtThreadsCount.Size = New System.Drawing.Size(61, 20)
+        Me.txtThreadsCount.TabIndex = 4
+        '
+        'ButStart
+        '
+        Me.ButStart.Location = New System.Drawing.Point(289, 67)
+        Me.ButStart.Name = "ButStart"
+        Me.ButStart.Size = New System.Drawing.Size(75, 23)
+        Me.ButStart.TabIndex = 5
+        Me.ButStart.Text = "Start"
+        Me.ButStart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 106)
+        Me.Controls.Add(Me.ButStart)
+        Me.Controls.Add(Me.txtThreadsCount)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtURL)
+        Me.Name = "Form1"
+        Me.Text = "Pages Downloader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtURL As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents txtThreadsCount As System.Windows.Forms.TextBox
+    Friend WithEvents ButStart As System.Windows.Forms.Button
+
+End Class
---
+++ LinkExtractor/Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Net
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim linksHT As Hashtable
+    Private Sub ButStart_Click(sender As System.Object, e As System.EventArgs) Handles ButStart.Click
+        ExtractLinks(txtURL.Text)
+    End Sub
+    Public Function ExtractLinks(ByVal url As String) As DataTable
+        Dim dt As New DataTable
+        dt.Columns.Add("LinkText")
+        dt.Columns.Add("LinkUrl")
+
+        Dim wc As New WebClient
+        Dim html As String = wc.DownloadString(url)
+
+        Dim links As MatchCollection = Regex.Matches(html, "<a.*?href=""(.*?)"".*?>(.*?)</a>")
+
+        For Each match As Match In links
+            Dim dr As DataRow = dt.NewRow
+            Dim matchUrl As String = match.Groups(1).Value
+            'Ignore all anchor links
+            If matchUrl.StartsWith("#") Then
+                Continue For
+            End If
+            'Ignore all javascript calls
+            If matchUrl.ToLower.StartsWith("javascript:") Then
+                Continue For
+            End If
+            'Ignore all email links
+            If matchUrl.ToLower.StartsWith("mailto:") Then
+                Continue For
+            End If
+            'For internal links, build the url mapped to the base address
+            If Not matchUrl.StartsWith("http://") And Not matchUrl.StartsWith("https://") Then
+                matchUrl = MapUrl(url, matchUrl)
+            End If
+            'Add the link data to datatable
+            dr("LinkUrl") = matchUrl
+            dr("LinkText") = match.Groups(2).Value
+            dt.Rows.Add(dr)
+        Next
+
+        Return dt
+    End Function
+
+    Public Function MapUrl(ByVal baseAddress As String, ByVal relativePath As String) As String
+
+        Dim u As New System.Uri(baseAddress)
+
+        If relativePath = "./" Then
+            relativePath = "/"
+        End If
+
+        If relativePath.StartsWith("/") Then
+            Return u.Scheme + Uri.SchemeDelimiter + u.Authority + relativePath
+        Else
+            Dim pathAndQuery As String = u.AbsolutePath
+            ' If the baseAddress contains a file name, like ..../Something.aspx
+            ' Trim off the file name
+            pathAndQuery = pathAndQuery.Split("?")(0).TrimEnd("/")
+            If pathAndQuery.Split("/")(pathAndQuery.Split("/").Count - 1).Contains(".") Then
+                pathAndQuery = pathAndQuery.Substring(0, pathAndQuery.LastIndexOf("/"))
+            End If
+            baseAddress = u.Scheme + Uri.SchemeDelimiter + u.Authority + pathAndQuery
+
+            'If the relativePath contains ../ then
+            ' adjust the baseAddress accordingly
+
+            While relativePath.StartsWith("../")
+                relativePath = relativePath.Substring(3)
+                If baseAddress.LastIndexOf("/") > baseAddress.IndexOf("//" + 2) Then
+                    baseAddress = baseAddress.Substring(0, baseAddress.LastIndexOf("/")).TrimEnd("/")
+                End If
+            End While
+
+            Return baseAddress + "/" + relativePath
+        End If
+
+    End Function
+
+End Class
